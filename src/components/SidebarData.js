86eb73d621fa387a81cc2419ec38e7091e7fcb55
Gabriel Utino <110830104+Gabriel-Utino@users.noreply.github.com>
2024-02-28 11:28:04 -0300

    first commit

---
+++ src/components/SidebarData.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+import EmailIcon from '@mui/icons-material/Email';
+
+export const SidebarData = [
+    {
+        title: "Home",
+        icon: <HomeIcon />,
+        link: "/home",
+    },
+    {
+        title: "e-mail",
+        icon: <EmailIcon />,
+        link: "/e-mail",
+    },
+];
+
+export default SidebarData
